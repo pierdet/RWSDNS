0be3dce2affa3a6d0313aedb13e5ef2e9b169ee8
Pierdet <antemxz@hotmail.com>
2020-03-05 19:25:38 +0100

    Reworked DNSProvider, added Todos

--- RWSDNS/Common/DNSProvider.cs
+++ RWSDNS/Common/DNSProvider.cs
@@ -9,8 +9,45 @@ namespace RWSDNS.Api.Common
 {
     public class DNSProvider
     {
+        public ApiResult DeleteARecord(string zone, string hostname, string ipAddress)
+        {
+            ManagementScope mgmtScope = new ManagementScope(@"\\.\Root\MicrosoftDNS");
+            ManagementClass mgmtClass = null;
+            ManagementBaseObject mgmtParams = null;
+            ManagementObjectSearcher mgmtSearch = null;
+            ManagementObjectCollection mgmtDNSRecords = null;
+            string strQuery;
+
+            strQuery = string.Format("SELECT * FROM MicrosoftDNS_AType WHERE OwnerName = '{0}.{1}'", hostname, zone);
+
+            mgmtScope.Connect();
+
+            mgmtSearch = new ManagementObjectSearcher(mgmtScope, new ObjectQuery(strQuery));
+
+            mgmtDNSRecords = mgmtSearch.Get();
+
+            // Multiple A records with the same record name, but different IPv4 addresses, skip.
+            if (mgmtDNSRecords.Count > 1)
+            {
+                // Todo - Implement Delete functionality
+                return new ApiResult { Success = false };
+            }
+            // Existing A record found, update record.
+            else if (mgmtDNSRecords.Count == 1)
+            {
+                // Todo - Implement Delete functionality
+                return new ApiResult { Success = false };
+            }
+            // A record does not exist, create new record.
+            else
+            {
+                // Todo - Implement Delete functionality
+                return new ApiResult { Success = false };
+            }
+        }
         public ApiResult UpdateARecord(string zone, string hostname, string ipAddress)
         {
+            // Thanks https://blog.mikejmcguire.com!
             ManagementScope mgmtScope = new ManagementScope(@"\\.\Root\MicrosoftDNS");
             ManagementClass mgmtClass = null;
             ManagementBaseObject mgmtParams = null;
