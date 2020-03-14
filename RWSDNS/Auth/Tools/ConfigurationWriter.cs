3202b918e642d65cda43b29196c93aee8ca432a2
Pierdet <antemxz@hotmail.com>
2020-03-14 16:41:34 +0100

    Changes to ApiKey

--- RWSDNS/Auth/Tools/ConfigurationWriter.cs
+++ RWSDNS/Auth/Tools/ConfigurationWriter.cs
@@ -14,20 +14,11 @@ namespace RWSDNS.Api.Auth.Tools
             try
             {
 
-                var filePath = Path.Combine(AppContext.BaseDirectory, "appSettings.json");
+                var filePath = Path.Combine(AppContext.BaseDirectory, "appsettings.json");
                 string json = File.ReadAllText(filePath);
                 dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
-
-                var sectionPath = key.Split(":")[0];
-                if (!string.IsNullOrEmpty(sectionPath))
-                {
-                    var keyPath = key.Split(":")[1];
-                    jsonObj[sectionPath][keyPath] = value;
-                }
-                else
-                {
-                    jsonObj[sectionPath] = value; // if no sectionpath just set the value
-                }
+                jsonObj[key] = value;
+                
                 string output = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObj, Newtonsoft.Json.Formatting.Indented);
                 File.WriteAllText(filePath, output);
 
