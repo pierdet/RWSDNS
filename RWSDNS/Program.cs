2e1fdf22bdabcd8071c00c4b45be7dc979a61487
Pierdet <antemxz@hotmail.com>
2020-03-14 16:54:46 +0100

    Changes to startup and program.cs to support on the fly change in appsettings.json

--- RWSDNS/Program.cs
+++ RWSDNS/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace RWSDNS.Api
@@ -12,9 +13,13 @@ namespace RWSDNS.Api
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((config =>
+                   config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
+                ))
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    
                 });
     }
 }
--- RWSDNS/Startup.cs
+++ RWSDNS/Startup.cs
@@ -10,6 +10,7 @@ namespace RWSDNS.Api
 {
     public class Startup
     {
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -29,6 +30,7 @@ namespace RWSDNS.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+            
             app.UseRouting();
 
             app.UseOpenApi();
