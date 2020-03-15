dacfb1d45dfbed288cfe2b6f758862650d659067
Pierdet <antemxz@hotmail.com>
2020-03-15 11:34:12 +0100

    Added authentication to all the controllers

--- RWSDNS/Controllers/TxtRecordController.cs
+++ RWSDNS/Controllers/TxtRecordController.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RWSDNS.Api.Auth;
 using RWSDNS.Api.Models;
 using RWSDNS.Api.Services;
 
 namespace RWSDNS.Api.Controllers
 {
+    [ApiVersion("1.0")]
     [Route("api/v{version:apiVersion}/dns/[controller]")]
     [ApiController]
+    [ApiKeyAuth]
     public class TxtRecordController : ControllerBase
     {
         private readonly IDNSService _dns;
